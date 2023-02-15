cc0f6d895ba3e1c9b876f804b7dd96ea2050cc98
Yurii Chertkov <yuriichertkov@gmail.com>
2023-02-15 15:49:38 +0300

    some tweaks to list of tasks

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import AppHeader from "./components/AppHeader";
 import AppTitle from "./components/AppTitle";
 import ListOfTasks from "./components/ListOfTasks";
-import style from "./styles/modules/app.module.scss";
+import styles from "./styles/modules/app.module.scss";
 
 function App() {
   return (
-    <div className="">
+    <div className="container">
       <AppTitle> TODO LIST</AppTitle>
-      <div className={style.app__wrapper}>
+      <div className={styles.app__wrapper}>
         <AppHeader />
-        </div>
-      <ListOfTasks />
+        <ListOfTasks />{" "}
+      </div>
     </div>
   );
 }
--- src/components/ListOfTasks.jsx
+++ src/components/ListOfTasks.jsx
@@ -3,8 +3,8 @@ import TaskItem from "./TaskItem";
 
 const ListOfTasks = () => {
   return (
-    <div className="px-5 py-6 z-0 container mx-auto border rounded-xl border-purple-300">
-      <div>ListOfTasks1</div>
+    <div className="">
+      <div>AppContent</div>
       <div>
         <TaskItem />
       </div>
